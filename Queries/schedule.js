a3d84fcaa0c80247447c8edb1c994511e585245b
elisaul batista <elisaulbatista@pursuit.org>
2024-07-28 20:25:26 -0400

    added query functions for schedule

--- Queries/schedule.js
+++ Queries/schedule.js
@@ -35,8 +35,42 @@ const getOneSchedule = async (id) => {
   }
 };
 
+const updateSchedule = async (id, schedule) => {
+  try {
+    const { barber_id, day_of_week, start_time, end_time } = schedule;
+    const updatedSchedule = await db.one(
+      "UPDATE barber_schedules SET barber_id=$1,day_of_week=$2,start_time=$3,end_time=$4 WHERE id=$5 RETURNING *"[
+        (barber_id, day_of_week, start_time, end_time, id)
+      ]
+    );
+    return updatedSchedule;
+  } catch (error) {
+    console.error("Error updating Schedule", error);
+    throw error;
+  }
+};
+
+const createSchedule = async (schedule) => {
+  try {
+    const createdSchedule = await db.one(
+      "INSERT INTO barber_schedules(barber_id, day_of_week , start_time , end_time) VALUES ($1,$2,$3,$4) RETURNING *",
+      [
+        schedule.barber_id,
+        schedule.day_of_week,
+        schedule.start_time,
+        schedule.end_time,
+      ]
+    );
+    return createdSchedule;
+  } catch (err) {
+    console.error("Error creating Schedule", err);
+    throw err;
+  }
+};
 module.exports = {
   getAllSchedule,
   getOneSchedule,
-  deleteSchedule
+  deleteSchedule,
+  createSchedule,
+  updateSchedule
 };
